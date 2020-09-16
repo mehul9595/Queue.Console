4a41782ee186dca8aa2276b4576415051806b217
mehul9595 <mehul9595@gmail.com>
2020-09-16 22:23:45 +0800

    add code for sending messages to the queue.

---
+++ QueueSend/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using Microsoft.Azure.ServiceBus;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace QueueSendConsole.Core
+{
+    class Program
+    {
+        private const string _queueName = "appqueue";
+
+        static async Task Main(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile(@"D:\Projects\Azure Labs\Queue.Console\QueueSend\appSettings.json").Build();
+            
+            Console.WriteLine($"{configuration["connectionString"]}");
+            
+            IQueueClient client = new QueueClient(configuration["connectionString"], _queueName);
+            
+            int i = 1;
+            while (i <= 1000)
+            {
+                string message = $"Sending Message: {i} - {DateTime.Now.ToString("yyyyMMddHHmmss")}";
+                Message msg = new Message(Encoding.UTF8.GetBytes(message));
+                msg.TimeToLive = TimeSpan.FromSeconds(30);
+                await client.SendAsync(msg);
+                await Console.Out.WriteLineAsync($"sending {message}");
+                i++;            
+            }
+
+            await Console.Out.WriteLineAsync("Hello World!");
+            Console.Read();
+        }
+    }
+}
