39e5417f643394d4743199c0a6268e304cd92051
mehul9595 <mehul9595@gmail.com>
2020-09-16 22:22:47 +0800

    adding code for queue receive

---
+++ QueueReceive/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.Azure.ServiceBus;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace QueueReceiveConsole.Core
+{
+    class Program
+    {
+        const string _queueName = "appqueue";
+        private static IQueueClient _client;
+        
+        static void Main(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile(@"D:\Projects\Azure Labs\Queue.Console\QueueReceive\appSettings.json").Build();
+            
+            Console.WriteLine($"{configuration["connectionString"]}");
+
+            _client = new QueueClient(configuration["connectionString"], _queueName);
+
+            MessageHandlerOptions options = new MessageHandlerOptions(ExceptionReceived)
+            {
+                AutoComplete = false,
+                MaxConcurrentCalls = 10
+            };
+
+            _client.RegisterMessageHandler(ProcessMessage, options);
+            Console.WriteLine("Reading Messages...");
+            Console.Read();           
+
+        }
+
+
+        static async Task ProcessMessage(Message message, CancellationToken cancellationToken)
+        {
+            await Console.Out.WriteLineAsync(Encoding.UTF8.GetString(message.Body));
+
+            await _client.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        static Task ExceptionReceived(ExceptionReceivedEventArgs args)
+        {
+            Console.WriteLine(args.Exception);
+            return Task.CompletedTask;
+        }
+
+
+    }
+}
